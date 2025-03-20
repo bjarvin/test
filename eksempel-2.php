cefd96be5a773c2c739033a42863dee5b6580c59
bjarvin <gb@usn.no>
2025-03-20 20:22:58 +0100

    Add files via upload

---
+++ eksempel-2.php
@@ -0,0 +1,16 @@
+<?php  /*  Eksempel 2 */
+/*
+/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
+/*    Programmet lager et fullt navn ved å inkludere en egendefinert fuksjon 
+*/
+
+  include("funksjoner.php");  /* funksjoner inkludert */
+
+  $fornavn=$_POST ["fornavn"];
+  $etternavn=$_POST ["etternavn"];  
+
+  $navn=fulltNavn($fornavn,$etternavn);
+
+  print ("Navnet er $navn");  
+
+?>
---
+++ eksempel-3.php
@@ -0,0 +1,24 @@
+<?php  /*  Eksempel 3 */
+/*
+/*    Programmet mottar postnr fra et HTML-skjema 
+/*    Programmet sjekker om postnr er korrekt fylt ut
+*/
+  $postnr=$_POST ["postnr"];
+
+  if (!$postnr)  /* postnr er ikke fylt ut */
+    {
+      print("Postnr er ikke fylt ut <br />");
+    }
+  else if (strlen($postnr)!=4)  /* postnr består ikke av 4 tegn */
+    {
+          print("Postnr består ikke av 4 tegn <br />");
+    }
+  else if (!ctype_digit($postnr))  /* minst ett av tegnene er ikke et siffer */
+    {
+       print("Postnr består ikke bare av siffre  <br />");
+    }
+else
+	{
+		print("Postnr er korrekt fylt ut <br />");
+	}
+?>
