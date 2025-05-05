7fe329a72d99139e4f21a7b52f1c6d7f1905a081
bjarvin <gb@usn.no>
2025-05-05 14:50:46 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -10,6 +10,13 @@ $database = getenv('DB_DATABASE');
   $db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
     /* tilkobling til database-serveren utført */
   print("tilkoblet db");
+
+  $sqlSetning="CREATE TABLE klasse (klassekode CHAR(5) NOT NULL,klasssenavn VARCHAR(50) NOT NULL,studiumkode VARCHAR(50) NOT NULL,PRIMARY KEY (klassekode));";
+  
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; opprette tabell i databasen");
+    /* SQL-setning sendt til database-serveren */
+
+    print("tabell opprettet");
 ?>
 
 
