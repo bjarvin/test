8ae3061fa7bc03165813d21916ca27b9de9a5b55
bjarvin <gb@usn.no>
2025-05-05 15:25:54 +0200

    Add files via upload

--- 0-opprett-poststedtabell.php
+++ 0-opprett-poststedtabell.php
@@ -1,12 +1,13 @@
 <?php  /* vis-alle-poststeder */
 /*
 /*  Programmet skriver ut alle registrerte poststeder
-*/
-  include("db-tilkobling.php");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
-  
-  mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+*/  
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-  $sqlSetning=" CREATE TABLE poststed (postnr CHAR(4) NOT NULL, poststed VARCHAR(30) NOT NULL, PRIMARY KEY (postnr));";
+include("db-tilkobling.php");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
+
+
+  $sqlSetning="CREATE TABLE poststed (postnr CHAR(4) NOT NULL, poststed VARCHAR(30) NOT NULL, PRIMARY KEY (postnr));";
    
   
   $sqlResultat=mysqli_query($db,$sqlSetning);
