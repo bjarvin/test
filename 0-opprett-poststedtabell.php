1bcbdfedf2f61bfb5f61583b5d49a9510f04656f
bjarvin <gb@usn.no>
2025-05-05 18:28:04 +0200

    Add files via upload

--- 0-opprett-poststedtabell.php
+++ 0-opprett-poststedtabell.php
@@ -6,9 +6,11 @@
 include("db-tilkobling.php");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
 
 
-  $sqlSetning="CREATE TABLE poststed (postnr CHAR(4) NOT NULL, poststed VARCHAR(30) NOT NULL, PRIMARY KEY (postnr));";
+  $sqlSetning="CREATE TABLE IF NOT EXISTS poststed (postnr CHAR(4) NOT NULL, poststed VARCHAR(30) NOT NULL, PRIMARY KEY (postnr));";
    
   $sqlResultat=mysqli_query($db,$sqlSetning) or die("ikke mulig  opprette tabell");
     /* SQL-setning sendt til database-serveren */
  
+  print("Poststedtabell opprettet (hvis den ikke var opprettet tidligere)");
 ?>
+ 
