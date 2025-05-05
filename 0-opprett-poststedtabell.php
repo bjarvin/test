0546412217f7845b5865f90dc9f885caa935f75e
bjarvin <gb@usn.no>
2025-05-05 15:14:44 +0200

    Add files via upload

---
+++ 0-opprett-poststedtabell.php
@@ -0,0 +1,14 @@
+<?php  /* vis-alle-poststeder */
+/*
+/*  Programmet skriver ut alle registrerte poststeder
+*/
+  include("db-tilkobling.php");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
+  
+
+  $sqlSetning="CREATE TABLE klasse (klassekode CHAR(5) NOT NULL,klasssenavn VARCHAR(50) NOT NULL,studiumkode VARCHAR(50) NOT NULL,PRIMARY KEY (klassekode));";
+   
+  $sqlResultat=mysqli_query($db,$sqlSetning);
+    /* SQL-setning sendt til database-serveren */
+ 
+  print("tabell opprettet");
+?>
