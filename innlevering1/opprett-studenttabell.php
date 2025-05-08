61466d4300b7df4723bc5f33c9bca64ebfc5b8ae
bjarvin <gb@usn.no>
2025-05-08 13:50:38 +0200

    Update opprett-studenttabell.php

--- innlevering1/opprett-studenttabell.php
+++ innlevering1/opprett-studenttabell.php
@@ -7,7 +7,7 @@ $database = getenv('DB_DATABASE');
 
 $db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
 
-$sqlSetning="CREATE TABLE student (brukernavn CHAR(7) NOT NULL, fornavn VARCHAR(50) NOT NULL, etternavn VARCHAR(50) NOT NULL, klassekode CHAR(5) NOT NULL, PRIMARY KEY (brukernavn), FOREIGN KEY (klassekode) REFERENCES klasse (klassekode));";
+$sqlSetning="CREATE TABLE IF NOT EXISTS student (brukernavn CHAR(7) NOT NULL, fornavn VARCHAR(50) NOT NULL, etternavn VARCHAR(50) NOT NULL, klassekode CHAR(5) NOT NULL, PRIMARY KEY (brukernavn), FOREIGN KEY (klassekode) REFERENCES klasse (klassekode));";
    
 mysqli_query($db,$sqlSetning) or die("ikke mulig  opprette tabell");
   
